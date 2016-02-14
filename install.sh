abc36eb4b400058dee23adf7700d044117b6593a
Peter Wang <wangxu198709@gmail.com>
2016-02-14 05:51:43 -0800

    Add airline as the status line theme
    also install powerline fonts to enable nice looking.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 # Linuxmint 17.3
 #######################################################
 VIM_RC_TMP=${VIM_RC_TMP:-/tmp/vim-configuration}
+FONTS=${FONTS:-/tmp/fonts}
 echo "If root priviledge is asked, please give."
 if [ -z $(which yum) ];then
     sudo apt-get -y install git python vim exuberant-ctags
@@ -20,17 +21,13 @@ rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm -rf $VIM_RC_TMP
 git clone https://github.com/peter-wangxu/vim-configuration $VIM_RC_TMP
+rm -rf $FONTS
+git clone https://github.com/powerline/fonts $FONTS
+$FONTS/install.sh
 
 mv -v $VIM_RC_TMP/.vimrc_Vundle ~/.vimrc
 
-echo "Begin to install following plugins:
-gmarik/Vundle.vim
-tpope/vim-fugitive
-L9
-rstacruz/sparkup
-davidhalter/jedi-vim
-vim-scripts/Tagbar
-scrooloose/nerdtree
-altercation/vim-colors-solarized"
+echo "Begin to install following plugins:"
 vim +PluginInstall +qall
 echo "Everything Done!"
+echo "[Optional]Please change your console font to powerline."
