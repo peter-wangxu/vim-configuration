e95cedb308b8d4a79f1b6d2035eddb674dc0f0ff
Peter Wang <wangxu198709@gmail.com>
2016-02-17 17:04:21 +0800

    Add some specific font for nerdtree
    as title

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@
 #######################################################
 VIM_RC_TMP=${VIM_RC_TMP:-/tmp/vim-configuration}
 FONTS=${FONTS:-/tmp/fonts}
+USR_FONTS=${USR_FONTS:-~/.local/share/fonts/}
 echo "If root priviledge is asked, please give."
 if [ -z $(which yum) ];then
     sudo apt-get -y install git python vim exuberant-ctags
@@ -21,6 +22,15 @@ rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm -rf $VIM_RC_TMP
 git clone https://github.com/peter-wangxu/vim-configuration $VIM_RC_TMP
+
+# Download some special fonts for github
+mkdir -p $USR_FONTS
+wget -P $USR_FONTS https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/FontAwesome.otf
+wget -P $USR_FONTS https://github.com/jesseweed/seti-ui/raw/master/styles/_fonts/icomoon.ttf
+wget -P $USR_FONTS https://github.com/github/octicons/raw/master/octicons/octicons.ttf
+wget -P $USR_FONTS https://github.com/ryanoasis/powerline-extra-symbols/raw/master/patched-fonts/DroidSansMonoForPowerlinePlusNerdFileTypesMono.otf
+
+
 rm -rf $FONTS
 git clone https://github.com/powerline/fonts $FONTS
 $FONTS/install.sh
